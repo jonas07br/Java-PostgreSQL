d9f398e98b9edb5b432261547dc84027336e6c91
jonas07br <jonasrafa07@gmail.com>
2024-06-13 19:30:53 -0300

    update functions

--- src/main/java/dio/spring_data_jpa/controllers/UserController.java
+++ src/main/java/dio/spring_data_jpa/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +26,8 @@ public class UserController {
     }
 
     @GetMapping("/usuarios/{name}")
-    public User findByUserName(@PathVariable("name") String name){
-        if(repository.findByName(name).isPresent()){
-            return repository.findByName(name).orElse(new User());
-        }
-        else{
-            return new User();
-        }
+    public List<User> findByUserName(@PathVariable("name") String name){
+        return repository.findByNameContaining(name);
     }
     @DeleteMapping("/usuarios/{id}")
     public void deleteUser(@PathVariable("id")Integer id){
@@ -40,6 +36,11 @@ public class UserController {
 
     @PostMapping("/usuarios")
     public void postUser(@RequestBody User usuario){
+        repository.save(usuario);
+        
+    }
+    @PutMapping("/usuarios")
+    public void putUser(@RequestBody User usuario){
         repository.save(usuario);
     }
 }
