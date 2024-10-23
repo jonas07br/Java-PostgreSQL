3c43d1b787b699981e4142e22f37ba543f445783
jonas07br <jonasrafa07@gmail.com>
2024-10-23 12:21:25 -0300

    delete models

--- src/main/java/dio/spring_data_jpa/repository/LivrosRepository.java
+++
@@ -1,9 +0,0 @@
-package dio.spring_data_jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dio.spring_data_jpa.model.Livros;
-
-public interface LivrosRepository extends JpaRepository<Livros,Integer> {
-
-}
